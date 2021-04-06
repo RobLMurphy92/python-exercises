deba48d69029aff02e251f17351758caf134ce45
Robert <rob.l.murphy@gmail.com>
2021-04-06 13:43:02 -0500

    added in answers

---
+++ python_introduction_exercises.py
@@ -0,0 +1,25 @@
+print("HELLO, WORLD!")
+greeting = "I am attempting to do this problem correctly"
+print(greeting)
+######################## problems###############
+# 3.) Create a hello world program. Create a text file named python_introduction_exercises.py and write a program that prints "Hello, World!" to the console. 
+# Run this program from the command line.
+
+# RESULTS :
+# Roberts-Air:python-exercises robert_murphy$ python python_introduction_exercises.py 
+# HELLO, WORLD!
+# Roberts-Air:python-exercises robert_murphy$ 
+
+##4.)Inside of your hello world program, create a variable named greeting that contains the message that you will print to the console.
+# Run your script interactively and view the contents of the greeting variable.
+
+#Results:
+#Roberts-Air:python-exercises robert_murphy$ python python_introduction_exercises.py 
+#HELLO, WORLD!
+#I am attempting to do this problem correctly
+#Roberts-Air:python-exercises robert_murphy$ 
+
+#1.)#Create a notebook and name it my_first_notebook. 
+#Test out the jupyter notebook environment: execute at least 3 Python cells, 
+# and make some notes that describe 3 shortcuts for jupyter notebooks.
+
