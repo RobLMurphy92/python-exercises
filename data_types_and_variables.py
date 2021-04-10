22b43f88a0cbf357085547f56e79d13b67693972
Robert <rob.l.murphy@gmail.com>
2021-04-10 00:49:53 -0500

    updated

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -34,115 +34,115 @@
 ### What data type would best represent:
 
 
-A term or phrase typed into a search box? string
+#A term or phrase typed into a search box? string
 
 
-If a user is logged in? boolean
+#If a user is logged in? boolean
 
 
-A discount amount to apply to a user's shopping cart? float
+#A discount amount to apply to a user's shopping cart? float
 
 
-Whether or not a coupon code is valid? boolean
+#Whether or not a coupon code is valid? boolean
 
 
-An email address typed into a registration form? dictionary
+#An email address typed into a registration form? dictionary
 
 
-The price of a product? float
+#The price of a product? float
 
 
-A Matrix? int 
+#A Matrix? int 
 
 
-The email addresses collected from a registration form? list of strings, 
+#The email addresses collected from a registration form? list of strings, 
 
 
-Information about applicants to Codeup's data science program? 
-dictionaries
+#Information about applicants to Codeup's data science program? 
+#dictionaries
 
 ### Exercise 3
 
 
-For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in your Python REPL.
+#For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in your Python REPL.
 
 
-'1' + 2 error
+#'1' + 2 error
 
 
-6 % 4 remainder of 2
+#6 % 4 remainder of 2
 
 
-type(6 % 4) int
+#type(6 % 4) int
 
 
-type(type(6 % 4)) type
+#type(type(6 % 4)) type
 
 
-'3 + 4 is ' + 3 + 4 error
+#'3 + 4 is ' + 3 + 4 error
 
-0 < 0 false
+#0 < 0 false
 
-'False' == False false
+#'False' == False false
 
-True == 'True' false
+#True == 'True' false
 
-5 >= -5 true
-
-
-True or "42" true
+#5 >= -5 true
 
 
-6 % 5 remainder 1
+#True or "42" true
 
 
-5 < 4 and 1 == 1 false
+#6 % 5 remainder 1
 
 
-'codeup' == 'codeup' and 'codeup' == 'Codeup' false
+#5 < 4 and 1 == 1 false
 
 
-4 >= 0 and 1 !== '1' error
+#'codeup' == 'codeup' and 'codeup' == 'Codeup' false
 
 
-6 % 3 == 0 true 
+#4 >= 0 and 1 !== '1' error
 
 
-5 % 2 != 0 true 
+#6 % 3 == 0 true 
 
 
-[1] + 2 error
+#5 % 2 != 0 true 
 
 
-[1] + [2]     [1,2]
+#[1] + 2 error
 
 
-[1] * 2 [1,1]
+#[1] + [2]     [1,2]
 
 
-[1] * [2] error cant mutiply sequence by non int.
+#[1] * 2 [1,1]
 
 
-[] + [] == [] true
+#[1] * [2] error cant mutiply sequence by non int.
 
 
-{} + {}     unsupported operation
+#[] + [] == [] true
+
+
+#{} + {}     unsupported operation
 
 
 ### Exercise data_types_and_variables
 
-Create a Python script file named data_types_and_variables.py. Inside it, write some Python code, that is, variables and operators, to describe the following scenarios. Do not worry about the real operations to get the values, the goal of these exercises is to understand how real world conditions can be represented with code.
+#Create a Python script file named data_types_and_variables.py. Inside it, write some Python code, that is, variables and operators, to describe the following scenarios. Do not worry about the real operations to get the values, the goal of these exercises is to understand how real world conditions can be represented with code.
 
-You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is 3 dollars, how much will you have to pay?
-
-
-Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380, and Facebook 350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+#You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is 3 dollars, how much will you have to pay?
 
 
-A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
+#Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays 400 dollars per hour, Amazon 380, and Facebook 350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
 
 
-A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+#A student can be enrolled to a class only if the class is not full and the class schedule does not conflict with her current schedule.
+
+
+#A product offer can be applied only if people buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
 
 # 1.
 #You have rented some movies for your kids: 
@@ -224,21 +224,17 @@ premium_membership = False
 discount_elig = offer_not_expired and (more_than_two_items or premium_membership)
 discount_elig
 
-Use the following code to follow the instructions below:
+#Use the following code to follow the instructions below:
 
 username = 'codeup'
 password = 'notastrongpassword'
 
 
-Create a variable that holds a boolean value for each of the following conditions:
-
-the password must be at least 5 characters
-
-the username must be no more than 20 characters
-
-the password must not be the same as the username
-
-bonus neither the username or password can start or end with whitespace
+#Create a variable that holds a boolean value for each of the following conditions:
+#the password must be at least 5 characters
+#the username must be no more than 20 characters
+#the password must not be the same as the username
+#bonus neither the username or password can start or end with whitespace
 
 username = 'codeup' 
 password = 'notastrongpassword'
